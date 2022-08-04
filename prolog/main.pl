5fb9c800abbc806d935dbd2e106f8a39b4a80dc1
Felipe Sarmento <felipeleaosarmento@gmail.com>
2022-08-04 10:56:16 -0300

    creating some util functions and added cpf validation

--- prolog/main.pl
+++ prolog/main.pl
@@ -1,3 +1,60 @@
+
+/*                              Predicados Úteis                                  */
+
+
+stringToList(S, R):- atom_chars(S, R).
+
+checkIfIsListOfNumber([], F):- F is 1.
+checkIfIsListOfNumber(F):- F is 0.
+checkIfIsListOfNumber([H | T], F):- 
+  (number(H) -> checkIfIsListOfNumber(T, F);
+  checkIfIsListOfNumber(F)).
+
+convertListOfStringsToListOfInt([], []). 
+convertListOfStringsToListOfInt([H|Ts], [Elm|Es]) :-
+   string_to_atom(H, Temp),
+   (atom_number(Temp, Elm) -> convertListOfStringsToListOfInt(Ts, Es); 
+   convertListOfStringsToListOfInt([], [])). 
+
+checkLenOfList(Xs, R) :- checkLenOfList(Xs, 0, R) .
+checkLenOfList( [], L, L).
+checkLenOfList( [_|Xs], T, R ) :-
+  T1 is T+1 ,
+  checkLenOfList(Xs, T1, R).
+
+/*      Regra de Negócio     */
+
+
+checkValidCpf(CPF):- 
+  stringToList(CPF, CPFList),
+  checkLenOfList(CPFList, LenResult),
+  convertListOfStringsToListOfInt(CPFList, R2),
+  checkIfIsListOfNumber(R2, F),
+  isCpfValid(LenResult, F).
+
+isCpfValid(L, F):-
+  (L is 11, F is 1 -> print('CPF COM SUCESSO!'); 
+  write('Erro, cpf inválido'), nl).
+
+menuBoasVindasHandler():-
+  menuBoasVindas(),
+  nl,
+  read(I),
+  (I is 1 -> nl, menuCriarConta(); 
+  I is 2 -> print('negativo'), nl;
+  write('ERRO!'), nl).
+
+
+menuCriarConta():-
+  digiteSeuCpfString(), nl,
+  read(CPF), nl,
+  checkValidCpf(CPF).
+
+
+
+
+/*                                    Strings                                                           */
+
 menuBoasVindas():-
   nl,
   print('Seja bem-vindo ao Festival Endpoint!'), nl,
@@ -5,13 +62,11 @@ menuBoasVindas():-
   print('(1) - Sim'), nl,
   print('(2) - Não'), nl.
 
-menuBoasVindasHandler():-
-  menuBoasVindas(),
-  nl,
-  read(I),
-  (I is 1 -> print('positivo'), nl; 
-  I is 2 -> print('negativo'), nl;
-  write('ERRO!'), nl).
+digiteSeuCpfString():-
+  print('Digite seu CPF (Apenas números)'), nl.
+
+digiteSuaSenhaString():-
+  print('Digite sua senha (no mínimo 6 dígitos)'), nl.
 
 main:-
   menuBoasVindasHandler().
