290d548c84d67092754c038abc9465f861c5f3ed
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-08-08 17:37:05 -0300

    Adiciona funcao para checar se o ingresso comprado é válido

--- prolog/util.pl
+++ prolog/util.pl
@@ -1,10 +1,12 @@
-- module(util, [login/2, isValidLogin/2]).
+- module(util, [login/2, isValidLogin/2, catalogoItensCompletos/2, catalogoItensIncompletos/2, leString/2]).
+
+leString(S):- read_line_to_string(user_input, S).
 
 login(Cpf, Senha):-
   write('Digite seu CPF (apenas números)'), 
-  read(Cpf),
+  leString(Cpf),
   write('Digite sua senha (no mínimo 6 caractéres) '), 
-  read(Senha).
+  leString(Senha).
 
 isValidLogin(Cpf, Senha):- 
   isValidCpf(Cpf),
@@ -26,3 +28,4 @@ checkCaracteresDoCpf([H | T]):-
   char_type(H, digit), 
   checkCaracteresDoCpf(T).
 
+ingressoValido(X):- member(X, ["L1", "L2", "L3", "R1", "R2", "R3"]).
