17315f5a29485d9282f9226daa3346c538ac055b
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-08-08 15:15:39 -0300

    Adiciona opcao de sair no menu inicial

--- prolog/main.pl
+++ prolog/main.pl
@@ -1,17 +1,22 @@
+:- ['./util.pl'].
+
 menuBoasVindas():-
   nl,
   print('Seja bem-vindo ao Festival Endpoint!'), nl,
   print('Você já possui uma conta?'), nl, nl,
   print('(1) - Sim'), nl,
-  print('(2) - Não'), nl.
+  print('(2) - Não'), nl,
+  print('(3) - Sair'), nl.
 
 menuBoasVindasHandler():-
   menuBoasVindas(),
   nl,
   read(I),
-  (I is 1 -> print('positivo'), nl; 
-  I is 2 -> print('negativo'), nl;
-  write('ERRO!'), nl).
+  (I is 1 -> write('TODO'), nl;                              % menu login 
+  I is 2 -> write('TODO'), nl;                              % menu criar conta
+  I is 3 -> abort;
+  write('OPÇÃO INVÁLIDA!'), nl,                              % opcao invalida
+  menuBoasVindasHandler).
 
 main:-
   menuBoasVindasHandler().
