27df92d6b91cdbe5d08bb3e172758ab76c97dc3a
João Vitor Mota <joao.mota@ccc.ufcg.edu.br>
2022-08-19 02:23:05 -0300

    Alterações para retornar o Menu Principal em algumas funcionalidades que estavam quebradas.

---
+++ prolog/data/bd_ingressos.pl
@@ -0,0 +1,18 @@
+
+:- dynamic ingresso/2.
+
+ingresso("cu", "L1").
+ingresso("cu", "L1").
+ingresso("cu", "L1").
+ingresso("cu", "L1").
+ingresso("cu", "L1").
+ingresso("cu", "L3").
+ingresso("cu", "L2").
+ingresso("cu", "L1").
+ingresso("cu", "L2").
+ingresso("cu", "L1").
+ingresso("cu", "L1").
+ingresso("cu", "L1").
+ingresso("cu", "L1").
+ingresso("cu", "L2").
+
