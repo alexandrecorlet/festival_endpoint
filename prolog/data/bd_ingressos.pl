c65e6560758e95c86d88d2494f151d26e2ab0eab
João Vitor Mota <joao.mota@ccc.ufcg.edu.br>
2022-08-19 14:02:56 -0300

    Funcionalidades básicas (1, 2, 4, 5 e 6) funcionando.

--- prolog/data/bd_ingressos.pl
+++ prolog/data/bd_ingressos.pl
@@ -2,19 +2,5 @@
 :- dynamic ingresso/2.
 
 ingresso("cpf", "L1").
-ingresso("cpf", "L1").
-ingresso("cu", "L1").
-ingresso("cu", "L1").
-ingresso("cu", "L1").
-ingresso("cu", "L1").
-ingresso("cu", "L1").
-ingresso("cu", "L3").
-ingresso("cu", "L2").
-ingresso("cu", "L1").
-ingresso("cu", "L2").
-ingresso("cu", "L1").
-ingresso("cu", "L1").
-ingresso("cu", "L1").
-ingresso("cu", "L1").
-ingresso("cu", "L2").
+ingresso("cpf", "R3").
 
